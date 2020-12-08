dc2cec978efa4195a771bc6464ea0db7479d1995
LianH <LianH@users.noreply.github.com>
2020-12-07 23:51:48 -0600

    Add files via upload

--- 2020/day7.R
+++ 2020/day7.R
@@ -40,3 +40,38 @@ while(length(beginning_outerbags) < length(outerbags)){
 length(outerbags)-(!count_shiny_gold)
 
 # Part 2
+
+split_line <- function(line, loc){
+  start <- gsub(", ", "", gsub("bags","bag", sub("[.]", "", substring(line, loc+13, nchar(line)))))
+  blub <- strsplit(start, " bag")
+  lapply(blub, function(x){strsplit(sub(" ", "|", x), "[|]")})
+}
+final_counts <- head(data.frame("color"=NA, "finalcount"=0),0)
+loop <- 1
+while(!"shiny gold" %in% final_counts$color){
+  print(loop)
+  loop <- loop+1
+for(line in input){
+  loc <- regexpr("bags contain",line)
+  color <- substring(line, 1, loc-2)
+  if(grepl("no other bags", line)){
+    final_counts <- rbind(final_counts, data.frame("color"=color, "finalcount"=1))
+  } else{
+    contained_colors <- split_line(line, loc)[[1]]
+    for(i in 1:length(contained_colors)){
+      precalc_count <- final_counts[final_counts$color==contained_colors[[i]][2],"finalcount"]
+      contained_colors[[i]][3] <- ifelse(length(precalc_count)==0, NA, precalc_count)
+    }
+    multipliers <- as.numeric(unlist(lapply(contained_colors, function(x){x[1]})))
+    bagcounts <- as.numeric(unlist(lapply(contained_colors, function(x){x[3]})))
+    if(all(!is.na(bagcounts))){
+      total_count <- sum(multipliers*bagcounts) + 1
+      final_counts <- rbind(final_counts, data.frame("color"=color, "finalcount"=total_count))
+      if(color=="shiny gold"){
+        break
+      }
+    }
+  }
+}
+}
+final_counts[final_counts$color=="shiny gold","finalcount"]-1 # Subtract one because for shiny gold we don't want to count itself
